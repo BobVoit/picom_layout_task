24ff7b487fa47143e643719fa096871e316f8aea
BobVoit <63784248+BobVoit@users.noreply.github.com>
2021-06-04 18:20:37 +0400

    update slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $('.slider').slick({
-        
+        dots: true,
+        appendArrows: $('.present__arrows'),
+        appendDots: $('.present__dots'),
+        customPaging: function (slider, i) {
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('slick-dots__body');
+            const element = document.createElement('button');
+            element.innerHTML = '0' + (i + 1);
+
+            wrapper.appendChild(element);
+            return wrapper;
+        }
     });
 })
